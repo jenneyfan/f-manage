108d21c69e3a870c5a0f484e23711de2c0d98984
Administrator <fanjunni2006@163.com>
2019-05-21 16:09:51 +0800

    表格操作完善（修改、删除）

--- src/components/page/BaseTable.vue
+++ src/components/page/BaseTable.vue
@@ -11,15 +11,18 @@
             <div class="handle-box">
                 <el-button type="primary" icon="delete" class="handle-del mr10" @click="delAll">批量删除</el-button>
                 <el-select v-model="selectCate" placeholder="筛选省份" class="handle-select mr10">
+                    <el-option key="0" label="全部" value=""></el-option>
                     <el-option key="1" label="广东省" value="广东省"></el-option>
                     <el-option key="2" label="湖南省" value="湖南省"></el-option>
+                    <el-option key="3" label="四川省" value="四川省"></el-option>
+                    <el-option key="4" label="江西省" value="江西省"></el-option>
                 </el-select>
                 <el-input v-model="selectWord" placeholder="筛选关键词" class="handle-input mr10"></el-input>
                 <el-button type="primary" icon="search" @click="search">搜索</el-button>
             </div>
             <el-table :data="data" border style="width: 100%" ref="multipleTable" @selection-change="handleSelectionChange">
-                <el-table-column type="selection" width="55"></el-table-column>
-                <el-table-column prop="date" label="日期" sortable width="150">
+                <el-table-column type="selection" width="35"></el-table-column>
+                <el-table-column prop="date" label="日期" sortable width="100">
                 </el-table-column>
                 <el-table-column prop="name" label="姓名" width="120">
                 </el-table-column>
@@ -27,7 +30,7 @@
                 </el-table-column>
                 <el-table-column label="操作" width="180">
                     <template slot-scope="scope">
-                        <el-button size="small" @click="handleEdit(scope.$index,scope.row)">编辑</el-button>
+                        <el-button size="small" @click="handleEdit(scope.$index, scope.row)">编辑</el-button>
                         <el-button size="small" type="danger" @click="handleDelete(scope.$index,scope.row)">删除</el-button>
                     </template>
                 </el-table-column>
@@ -36,6 +39,33 @@
                 <el-pagination @current-change="handleCurrentChange" layout="prev,pager, next" :total="1000"></el-pagination>
             </div>
         </div>
+        <!-- 编辑弹出框 -->
+        <el-dialog title="编辑" :visible.sync="editVisible" width="30%">
+            <el-form ref="form" :model="form" label-width="50px">
+                <el-form-item label="日期">
+                    <el-date-picker type="date" placeholder="选择日期" v-model="form.date" value-format="yyyy-MM-dd" style="width: 100%;"></el-date-picker>
+                </el-form-item>
+                <el-form-item label="姓名">
+                    <el-input v-model="form.name"></el-input>
+                </el-form-item>
+                <el-form-item label="地址">
+                    <el-input v-model="form.address"></el-input>
+                </el-form-item>
+            </el-form>
+            <span slot="footer" class="dialog-footer">
+                <el-button @click="editVisible = false">取 消</el-button>
+                <el-button type="primary" @click="saveEdit">确 定</el-button>
+            </span>
+        </el-dialog>
+        <!-- 删除提示框 -->
+        <el-dialog title="编辑" :visible.sync="delVisible" width="30%">
+            <div class="del-dialog-cnt">删除不可恢复，是否确定删除？</div>
+            <span slot="footer" class="dialog-footer">
+                <el-button @click="delVisible = false">取 消</el-button>
+                <el-button type="primary" @click="deleteRow">确 定</el-button>
+            </span>
+        </el-dialog>
+
     </div>
 </template>
 
@@ -87,8 +117,8 @@
         },
         methods:{
             handleCurrentChange(val){
-                this.multipleSelection = val;
-                console.log(this.multipleSelection);
+                this.curPage = val;
+                this.getData();
             },
             getData(){
                 axios.get(this.url,{
@@ -100,20 +130,43 @@
             search(){
                 this.isSearch = true;
             },
-            filterTag(value,row){
-
-            },
             handleEdit(index,row){
-
+                this.idx = index;
+                const item = this.tableData[index];
+                this.form = {
+                    name: item.name,
+                    date: item.date,
+                    address: item.address,
+                },
+                this.editVisible = true;
             },
             handleDelete(index,row){
-
+                this.idx = index;
+                this.delVisible = true;
             },
             delAll(){
-
+                const length = this.multipleSelection.length;
+                let str = '';
+                this.delList = this.delList.concat(this.multipleSelection);
+                for(let i = 0; i < length; i++){
+                    str += this.multipleSelection[i].name + ','
+                }
+                str = str.substring(0, str.length-1);
+                this.$message.error('删除了 {' + str + '}');
+                this.multipleSelection = [];
             },
-            handleSelectionChange(){
-
+            handleSelectionChange(val){
+                this.multipleSelection = val;
+            },
+            saveEdit(){
+                this.$set(this.tableData, this.idx, this.form);
+                this.editVisible = false;
+                this.$message.success(`修改${this.idx+1}行成功`);
+            },
+            deleteRow(){
+                this.tableData.splice(this.idx,1);
+                this.$message.success('删除成功');
+                this.delVisible = false;
             }
         }
     }
