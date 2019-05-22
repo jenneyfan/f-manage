b5e8a1cc526020f65a155541545b93db6a8f912c
Administrator <fanjunni2006@163.com>
2019-05-22 14:38:39 +0800

    tab选项卡功能完善

--- src/components/page/Tabs.vue
+++ src/components/page/Tabs.vue
@@ -24,7 +24,7 @@
                         </el-table-column>
                     </el-table>
                     <div class="handle-row">
-                        <el-button type="primary">全部标为已读</el-button>
+                        <el-button type="primary" @click="handleReadAll">全部标为已读</el-button>
                     </div>
                 </el-tab-pane>
                 <el-tab-pane :label="`已读消息(${list.read.length})`" name="second">
@@ -43,13 +43,13 @@
                             </el-table-column>
                         </el-table>
                         <div class="handle-row">
-                            <el-button type="danger">全部删除</el-button>
+                            <el-button type="danger" @click="handleDelAll">全部删除</el-button>
                         </div>
                     </template>
                 </el-tab-pane>
                 <el-tab-pane :label="`回收站(${list.recycle.length})`" name="third">
                     <template v-if="message === 'third'">
-                        <el-table :data="list.read" :show-header="false" style="width: 100%">
+                        <el-table :data="list.recycle" :show-header="false" style="width: 100%">
                             <el-table-column>
                                 <template slot-scope="scope">
                                     <span class="message-title">{{scope.row.title}}</span>
@@ -63,7 +63,7 @@
                             </el-table-column>
                         </el-table>
                         <div class="handle-row">
-                            <el-button type="danger">清空回收站</el-button>
+                            <el-button type="danger" @click="handleRestoreAll">清空回收站</el-button>
                         </div>
                     </template>
                 </el-tab-pane>
@@ -101,16 +101,30 @@
         },
         methods:{
             handleRead(idx){
-                const item = this.list.unread.splice(idx,1);
-                this.list.read = item.concat(this.list.read);
+                this.changeArr('unread','read',idx);
             },
             handleDel(idx){
-                const item = this.list.read.splice(idx,1);
-                this.list.recycle = item.concat(this.list.recycle);
+                this.changeArr('read','recycle',idx);
             },
             handleRestore(idx){
-                const item = this.list.recycle.splice(idx,1);
-                this.list.read = item.concat(this.list.read);
+                this.changeArr('recycle','read',idx);
+            },
+            handleReadAll(){
+                this.concatArr('unread','read');
+            },
+            handleDelAll(){
+                this.concatArr('read','recycle');
+            },
+            handleRestoreAll(){
+                this.concatArr('recycle','read');
+            },
+            changeArr(arr1,arr2,idx){
+                const item = this.list[arr1].splice(idx,1);
+                this.list[arr2] = item.concat(this.list[arr2]);
+            },
+            concatArr(arr1,arr2){
+                this.list[arr2] = this.list[arr1].concat(this.list[arr2]);
+                this.list[arr1] = [];
             }
         }
     }
