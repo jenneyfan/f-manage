cfb369f357690c9158eb9d5ca560695c95f19452
Administrator <fanjunni2006@163.com>
2019-05-24 14:45:33 +0800

    富文本编辑器 mavonEditor

--- src/components/page/Markdown.vue
+++ src/components/page/Markdown.vue
@@ -1,15 +1,56 @@
 <template>
     <div>
-        Markdown
+        <div class="crumbs">
+            <el-breadcrumb separator="/">
+                <el-breadcrumb-item><i class="el-icon-date"></i> 表单 </el-breadcrumb-item>
+                <el-breadcrumb-item> markdown编辑器 </el-breadcrumb-item>
+            </el-breadcrumb>
+        </div>
+        <div class="container">
+            <div class="plugins-tips">
+                mavonEditor：基于Vue的markdown编辑器。
+                访问地址：<a href="https://github.com/hinesboy/mavonEditor" target="_blank">mavonEditor</a>
+            </div>
+            <mavon-editor v-model="content" ref="md" @imgAdd="$imgAdd" @change="change" style="min-height: 500px"></mavon-editor>
+            <el-button class="editor-btn" type="primary" @click="submit">提交</el-button>
+        </div>
     </div>
 </template>
 
 <script>
+    import axios from 'axios';
+    import {mavonEditor} from 'mavon-editor';
+    import 'mavon-editor/dist/css/index.css';
     export default {
+        components: {mavonEditor},
         name:'markdown',
         data() {
             return {
-                msg: 'Welcome to Your Vue.js App'
+                content:'',
+                html:'',
+                configs:{}
+            }
+        },
+        methods:{
+            $imgAdd(pos,$file){
+                var formdata = new FormData();
+                formdata.append('file',$file);
+                axios({
+                    url:'',
+                    method:'post',
+                    data:formdata,
+                    headers:{'Content-Type':'multipart/form-data'}
+                }).then((url)=>{
+                    this.$refs.md.$img2Url(pos,url)
+                });
+            },
+            change(value,render){
+                this.html = render;
+            },
+            submit(){
+                console.log(this.content);
+                console.log(this.html);
+                this.$message.success('提交成功！');
             }
         }
     }
