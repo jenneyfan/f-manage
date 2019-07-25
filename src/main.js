c563f7671901d87632baf3a0c12ea8979244f263
Jenney <fanjunni2006@163.com>
2019-07-25 17:38:45 +0800

    Vuex初始化问题

--- src/main.js
+++ src/main.js
@@ -15,7 +15,7 @@ const store = new Vuex.Store({
     state:{
         user:'admin',
         role:'系统管理员',
-        loginDate:'第一次登录',
+        loginDate: localStorage.getItem('lastDate') || '第一次登录',
         msgCount:2
     },
     mutations:{
